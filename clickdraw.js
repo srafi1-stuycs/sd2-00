3939a40e6f9ba31b3204bc907d9584f69d8bc699
srafi1 <shakilrafi0@gmail.com>
2018-02-05 18:24:07 -0500

    connect the dots

--- clickdraw.js
+++ clickdraw.js
@@ -1,4 +1,4 @@
-var c, ctx, modeSpan, drawState, width, height;
+var c, ctx, modeSpan, drawState, width, height, startedLine, lastCoords;;
 
 function main() {
     console.log("setting up canvas");
@@ -10,6 +10,12 @@ function main() {
     drawState = 0;
     width = 600;
     height = 600;
+
+    startedLine = false;
+    lastCoords = {
+        x: 0,
+        y: 0
+    };
 }
 
 function toggle() {
@@ -32,15 +38,29 @@ function toggle() {
 function clearScreen() {
     console.log("clearing screen");
     ctx.clearRect(0, 0, width, height);
+    startedLine = false;
 }
 
 function drawObject(e) {
+    e.preventDefault();
     switch(drawState) {
         case 0:
             console.log("drawing circle at " + e.offsetY + ", " + e.offsetY);
             ctx.beginPath();
             ctx.arc(e.offsetX, e.offsetY, 20, 0, Math.PI*2);
             ctx.fill();
+            if (startedLine) {
+                ctx.beginPath();
+                ctx.moveTo(lastCoords.x, lastCoords.y);
+                ctx.lineTo(e.offsetX, e.offsetY);
+                ctx.stroke();
+                lastCoords.x = e.offsetX;
+                lastCoords.y = e.offsetY;
+            } else {
+                lastCoords.x = e.offsetX;
+                lastCoords.y = e.offsetY;
+                startedLine = true;
+            }
             break;
         case 1:
             console.log("drawing rectangle at " + e.offsetY + ", " + e.offsetY);
